e31e4cbe52c5f65cee248b2b42fd95093195e412
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-10-17 01:52:54 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo, Please Wait..."
+git clone https://github.com/deepukumarpuri/filetolink /filetolink
+cd /filetolink
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 main.py
